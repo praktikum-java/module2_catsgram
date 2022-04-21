49acd1e373f941e6e90c353bc306819e0cebb04f
PracticumJava <ragozina.anastasiia@yandex.ru>
2022-04-21 20:48:16 +0300

    prepare for error handling

--- src/main/java/ru/yandex/practicum/catsgram/exception/PostNotFoundException.java
+++ src/main/java/ru/yandex/practicum/catsgram/exception/PostNotFoundException.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.catsgram.exception;
 
 public class PostNotFoundException extends RuntimeException {
-
     public PostNotFoundException(String message){
         super(message);
     }
